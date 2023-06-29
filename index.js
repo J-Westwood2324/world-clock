369f0483ea275f7de375c4e4193ad95078a98851
Jade <jade-westwood@outlook.com>
2023-06-29 21:46:39 +0100

    Added real time data to select options

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ function updateIndexTime() {
   let moscowTime = moment().tz("Europe/Moscow");
   moscowDateElement.innerHTML = moscowTime.format("Do MMMM YYYY");
   moscowTimeElement.innerHTML = moscowTime.format("LTS");
+
+  let cancunElement = document.querySelector("#cancun");
+  let cancunDateElement = cancunElement.querySelector(".date");
+  let cancunTimeElement = cancunElement.querySelector(".time");
+  let cancunTime = moment().tz("America/Cancun");
+  cancunDateElement.innerHTML = cancunTime.format("Do MMMM YYYY");
+  cancunTimeElement.innerHTML = cancunTime.format("LTS");
+}
+
+function cityTimeChange(event) {
+  let timeZone = event.target.value;
+  let cityTimeZone = moment().tz(timeZone);
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let worldClock = document.querySelector("#indexCities");
+  worldClock.innerHTML = `
+         <div class="city">
+          <h2>${cityName}</h2>
+          <div class="date">${cityTimeZone.format("Do MMMM YYYY")}</div>
+          <div class="time">${cityTimeZone.format("LTS")}</div>`;
 }
 
 updateIndexTime();
 setInterval(updateIndexTime, 1000);
+
+let selectElement = document.querySelector("#cities");
+selectElement.addEventListener("change", cityTimeChange);
